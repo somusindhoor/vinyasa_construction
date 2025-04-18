745185174e7f98c8bb220fd94a7a4a4111fff24d
somusindhoor <somusindhoor1234@gmail.com>
2025-04-18 18:45:47 +0530

    Ref: router remove

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 // Import necessary components from react-router-dom
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Dashboard from './Dashboard'; // Your existing Dashboard component
 
 function App() {
   return (
-    <Router basename='vinyasa_construction'>
-        {/* You can add a navbar or any other component here */}
-        <Routes>
-          {/* Define routes here */}
-          <Route path="/" element={<Navigate to="/vinyasa" replace />} />
-          <Route path="/vinyasa" element={<Dashboard />} />
-        </Routes>
-    </Router>
+    <div className="App">
+    <Dashboard />
+  </div>
+    // <Router basename='vinyasa_construction'>
+    //     {/* You can add a navbar or any other component here */}
+    //     <Routes>
+    //       {/* Define routes here */}
+    //       <Route path="/" element={<Navigate to="/vinyasa" replace />} />
+    //       <Route path="/vinyasa" element={<Dashboard />} />
+    //     </Routes>
+    // </Router>
   );
 }
 
